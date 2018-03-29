8e458953a09f0b72e4fe8dfdc58de5b7657f350c
GordonBryden <gordon.bryden@gmail.com>
2018-03-29 07:48:39 +0000

    Update twitter_bot.r

--- twitter_bot.r
+++ twitter_bot.r
@@ -24,7 +24,7 @@ tweets <- (get_tweets[!is.na(get_tweets)])
 tweetframe<-as.data.frame(tweets,stringsAsFactors=FALSE)%>%
   mutate(tweetnumber=row_number())
 
-test<-tweetframe%>%
+wordfile<-tweetframe%>%
   unnest_tokens("word","tweets")%>%
   filter(!word %in% c(stop_words$word,
                       "rt",
@@ -33,24 +33,16 @@ test<-tweetframe%>%
                       "t.co"))
 
 
-#png("wordcloud.png",
-#    width=640,
-#    height=640)
+png("wordcloud.png",
+    width=640,
+    height=640)
 
-wordcloud(test$word,
+wordcloud(wordfile$word,
           max.words = 100,
           random.order = TRUE,
           control = list(removePunctuation = TRUE),
           colors=brewer.pal(8,"Dark2")
 )
-
-
-wordcloud(tweet_corpus,
-          max.words = 100,
-          random.order = FALSE,
-          control = list(removePunctuation = TRUE),
-          colors=brewer.pal(8,"Dark2")
-          )
 
 dev.off()
 
